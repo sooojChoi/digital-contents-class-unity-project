059d21dc817b982c5c6102b2fb38f242177e6d15
sooojChoi <cmleon88@naver.com>
2022-04-06 21:53:22 +0900

    firstSceneChanged

--- 1971144-Choisoojin/Assets/Scripts/FirstScene/CanvasScript.cs
+++ 1971144-Choisoojin/Assets/Scripts/FirstScene/CanvasScript.cs
@@ -32,7 +32,7 @@ public class CanvasScript : MonoBehaviour
         tempArray[1] = "모험가님, 소문을 듣고 오셨군요.";
         tempArray[2] = "보스 몬스터를 잡으면 엄청난 보물을 받을 수 있다는 소문 말이에요!";
         tempArray[3] = "그 몬스터 때문에 우리 마을 사람들 모두 겁에 질려 있어요.";
-        tempArray[4] = "모험가님, 보스 몬스터를 잡으신다면 저를 찾아주세요. 기다리고 있을게요.";
+        tempArray[4] = "모험가님, 보스 몬스터를 잡고 저를 찾아주세요. 기다리고 있을게요.";
 
         return tempArray;
     }
